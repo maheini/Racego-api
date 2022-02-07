7409db5705bb7dd6f839ddba3e331937d9fdfb60
maheini35 <maheini35@me.com>
2022-02-07 12:06:48 +0100

    cleanup & re-organisation

--- RacegoController.php
+++ RacegoController.php
@@ -16,11 +16,11 @@ class RacegoController {
 
     public function __construct(Router $router, Responder $responder, GenericDB $db, ReflectionService $reflection, Cache $cache)
     {
-        $router->register('GET', '/v1/user', array($this, 'getUser'));
+        $router->register('GET', '/v1/user', array($this, 'getUser'));          // rework!
+        $router->register('GET', '/v1/track', array($this, 'getTrack'));        // rework!
         $router->register('POST', '/v1/user', array($this, 'addUser'));
         $router->register('DELETE', '/v1/user', array($this, 'deleteUser'));
-        $router->register('GET', '/v1/track', array($this, 'getTrack'));
-        $router->register('PUT', '/v1/set_user', array($this, 'putUser'));
+        $router->register('PUT', '/v1/user', array($this, 'updateUser'));       // rework!
         $this->responder = $responder;
         $this->db = $db;
     }
@@ -30,6 +30,24 @@ class RacegoController {
         $sql = "SELECT user_id AS id, surname AS last_name, forname AS first_name, COUNT(lap_time) AS lap_count 
         FROM user LEFT JOIN laps ON user.user_id = laps.user_id_ref 
         GROUP BY forname, surname, user_id ORDER BY forname, surname, lap_count";
+
+        $pdo = $this->db->pdo();
+        $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute();
+
+        $record = $stmt->fetchAll() ?: null;
+        if ($record === null) {
+            return $this->responder->success([]);
+        }
+        return $this->responder->success($record);
+    }
+
+    public function getTrack(ServerRequestInterface $request)
+    {
+        $sql = "SELECT user.user_id AS id, user.forname AS first_name, user.surname AS last_name FROM track
+                LEFT JOIN user 
+                ON track.user_id_ref = user.user_id ORDER BY track.id";
 
         $pdo = $this->db->pdo();
         $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
@@ -143,25 +161,8 @@ class RacegoController {
         return $this->responder->success(['affected_rows' =>  $result->rowCount()]);
     }
 
-    public function getTrack(ServerRequestInterface $request)
-    {
-        $sql = "SELECT user.user_id AS id, user.forname AS first_name, user.surname AS last_name FROM track
-                LEFT JOIN user 
-                ON track.user_id_ref = user.user_id ORDER BY track.id";
 
-        $pdo = $this->db->pdo();
-        $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-        $stmt = $pdo->prepare($sql);
-        $stmt->execute();
-
-        $record = $stmt->fetchAll() ?: null;
-        if ($record === null) {
-            return $this->responder->success([]);
-        }
-        return $this->responder->success($record);
-    }
-
-    public function putUser(ServerRequestInterface $request)
+    public function updateUser(ServerRequestInterface $request)
     {
         //input validation
         $body = $request->getParsedBody();
@@ -192,34 +193,4 @@ class RacegoController {
         return $this->responder->success(['affected_rows' =>  $stmt->rowCount()]);
     }
     
-    /* 
-    public function setBookState($request)
-    {
-        $code_record_not_found = Tqdev\PhpCrudApi\Record\ErrorCode::RECORD_NOT_FOUND;
-        $code_validation_failed = Tqdev\PhpCrudApi\Record\ErrorCode::INPUT_VALIDATION_FAILED;
-        $body = $request->getParsedBody();
-        $id = (int)Tqdev\PhpCrudApi\RequestUtils::getPathSegment($request, 2);
-        $new_state = property_exists($body, 'new_state') ? $body->new_state : '';
-        $id = property_exists($body, 'id') ? $body->id : 0;
-        
-        if(id <= 0)
-        return $this->responder->error($code_record_not_found);
-        else return $this->responder->success(['answer' => 'Erfolgreich :)']);
- */
-/*         // Example how to get multiple records.
-        $data = $this->record_service->_list('book', ['filter'=>['author,eq,Adrian']]);
-        $books = $data->getRecords();
-        
-        // Example how to read one record
-        $book = $this->record_service->read('book', $id, []);
-        if (!$book) {
-            return $this->responder->error($code_record_not_found, $id);
-        }
-        $record = (object)[
-            'state' => 'new_state'
-        ];
-        return $this->responder->success($this->record_service->update( 'book', $id, $record, []));
-
-    } */
-
 }
